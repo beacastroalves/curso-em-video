d4507babf18b8b43bdeeb8cdff40592b8fbb3e5e
Beatriz Castro <beacastro1008@gmail.com>
2025-06-02 17:04:36 +0100

    exercicio condição if

--- javascript-aulas/mod-d/aula11/ex011.js
+++ javascript-aulas/mod-d/aula11/ex011.js
@@ -1,9 +1,25 @@
-var pais = 'Portugal';
+function ehEstrangeiro() {
+  var paisNasc = window.document.querySelector('#paisNasc');
+  var paisVive = window.document.querySelector('#paisVive');
+  var res = window.document.querySelector('#res');
+  var resPaisNasc = paisNasc.value;
+  var resPaisVive = paisVive.value;
 
-console.log(`Vivendo em ${Portugal}`);
+  res.innerHTML = `<p><strong>${resPaisNasc}</strong>: é o país onde eu nasci</p>  <p><strong>${resPaisVive}</strong>: é o país onde eu vivo</p>`;
 
-if (pais != 'Brasil') {
-  console.log('Você é Estrangeiro!');
-} else {
-  console.log('Você é Brasileiro!');
+  if (resPaisNasc === 'Brasil') {
+    res.innerHTML += '<p>Você é Brasileiro!</p>';
+
+    if (resPaisVive === resPaisNasc) {
+      res.innerHTML += '<p>E mora no Brasil!</p>';
+    } else {
+      res.innerHTML += '<p>E mora fora do Brasil!</p>';
+    }
+  } else {
+    res.innerHTML += '<p>Você é um estrangeiro!</p>';
+
+    if (resPaisVive === 'Brasil') {
+      res.innerHTML += '<p>E mora no Brasil!</p>';
+    }
+  }
 }
