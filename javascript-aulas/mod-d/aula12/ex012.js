5d510c29a4f715a8f6f07dee267589afead8e29e
Beatriz Castro <beacastro1008@gmail.com>
2025-06-03 12:26:22 +0100

    aula 12 js - pt1

---
+++ javascript-aulas/mod-d/aula12/ex012.js
@@ -0,0 +1,16 @@
+function podeVotar() {
+  var anoNasc = window.document.querySelector('#anoNasc');
+  var anoAtual = new Date().getFullYear();
+  var resIdade = anoAtual - Number(anoNasc.value);
+  var res = window.document.querySelector('#res');
+
+  if (resIdade < 16) {
+    res.innerHTML = `<p>Você tem ${resIdade} anos, é de menor de idade e <span>não pode votar</span>!</p>`;
+  } else if (resIdade < 18) {
+    res.innerHTML = `<p>Você tem ${resIdade} anos, é de menor de idade e <span>votar é opcional!</span></p>`;
+  } else if (resIdade <= 70) {
+    res.innerHTML = `<p>Você tem ${resIdade} anos, é de maior de idade e <span>votar é obrigatório!</span></p>`;
+  } else {
+    res.innerHTML = `<p>Você tem ${resIdade} anos, é de maior de idade e <span>votar é opcional!</span></p>`;
+  }
+}
