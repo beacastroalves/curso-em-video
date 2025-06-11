b04a824e2ec5776db199e2ca845189e352225574
beacastroalves <beacastro1008@gmail.com>
2025-06-11 16:42:03 +0100

    exercicio parte 2

--- javascript-aulas/mod-d/aula12/ex014.js
+++ javascript-aulas/mod-d/aula12/ex014.js
@@ -1,1 +1,20 @@
-alert("Olá!");
+function carregar() {
+  const msg = document.querySelector('#msg');
+  const img = document.querySelector('#imagem');
+  const data = new Date();
+  const hora = data.getHours();
+  msg.innerHTML = `Agora são ${hora} horas.`;
+  if(hora >= 0 && hora < 12) {
+    // Bom dia
+    img.src = 'morning.png';
+    document.body.style.background = '#debb6e';
+  } else if (hora >= 12 && hora < 18) {
+    // Boa tarde
+    img.src = 'afternoon.png';
+    document.body.style.background = '#b9846f';
+  } else {
+    // Boa noite
+    img.src = 'night.png';
+    document.body.style.background = '#515154';
+  }
+}
